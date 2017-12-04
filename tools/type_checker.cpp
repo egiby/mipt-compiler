02f86fe3c8034994a7a6d3ca1dc21ee0afe3e5f1
Vruchtel Serafima <vruchtel.serafima@yandex.ru>
2017-12-04 18:22:09 +0300

    #12 Add dependency checker

--- tools/type_checker.cpp
+++ tools/type_checker.cpp
@@ -1,1 +1,28 @@
+#include <ast/TreeBuilder.h>
+#include <symbol_table/SymbolTableCreatorVisitor.h>
+#include <type_checker/DependencyChecker.h>
 
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        std::cerr << "There must be one argument - minijava file for parsing" << std::endl;
+        return 0;
+    }
+
+    std::ifstream input(argv[1]);
+    auto program = NSyntaxTree::BuildTree(&input);
+
+    NSymbolTable::SymbolTable table;
+    NUtil::StringInterner interner;
+
+    NSymbolTable::SymbolTableCreatorVisitor creator(table, &interner);
+    creator.Visit(&program);
+
+    //std::cout << table.GetMainClassId()->String() << std::endl;
+
+    NTypeChecker::CheckDependencies(table);
+
+    return 0;
+}
--- type_checker/DependencyChecker.h
+++ type_checker/DependencyChecker.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <symbol_table/SymbolTable.h>
+#include <symbol_table/ClassInfo.h>
+#include <symbol_table/Exceptions.h>
 
 namespace NTypeChecker {
     void CheckDependencies(const NSymbolTable::SymbolTable& symbolTable);
+
+    void CheckInClass(const NSymbolTable::SymbolTable& symbolTable,
+        const NUtil::Symbol* superClassId,
+        const NSymbolTable::ClassInfo& childClassInfo);
 }
