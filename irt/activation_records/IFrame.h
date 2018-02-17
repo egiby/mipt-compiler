b08ac007ce7a01b35e62245988b39c393fb32795
egiby <andrey.upshinskiy@phystech.edu>
2018-02-17 15:47:22 +0300

    minimal code for activation records

--- irt/activation_records/IFrame.h
+++ irt/activation_records/IFrame.h
@@ -4,10 +4,15 @@
 
 #include <symbol_table/VariableInfo.h>
 
+#include <memory>
+
 namespace NIRTree {
     interface IFrame {
         virtual void AddLocal(const NSymbolTable::VariableInfo&) = 0;
         virtual void AddFormal(const NSymbolTable::VariableInfo&) = 0;
+        virtual int GetFormalsCount() const = 0;
+        virtual std::shared_ptr<const IAccess> GetFormal(int index) = 0;
+        virtual std::shared_ptr<const IAccess> GetFormalOrLocal(const NSymbolTable::Symbol *name) = 0;
 
         virtual ~IFrame() {
         }
---
+++ irt/activation_records/InFrameAccess.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "IAccess.h"
+
+namespace NIRTree {
+    class InFrameAccess: public IAccess {
+    };
+}
